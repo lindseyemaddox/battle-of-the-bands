805a361036d58aacf417aea0a14d52abf3225b43
Lindsey Maddox <dev@lindseymaddox.com>
2018-10-23 10:22:24 -0500

    noodling around on some methods

---
+++ main.js
@@ -0,0 +1,17 @@
+
+// Your job is to write a function that accepts any band name as an argument. 
+// The function will increment a global variable by one each time it is invoked, 
+// and return that number, and the band name concatenated together.
+
+const bandNumber = 1
+
+const takeNumber = function assignNumber (...restArgs) {
+  for (bandNumber = 1; bandNumber >= 1; bandNumber++) => i
+  forEach bandNumber (restArgs.length);
+}
+
+const scum = takeNumber("Galactic Scum")
+console.log(scum)  // This should print "1. Galactic Scum" in the console
+
+const under = takeNumber("Underdogs")
+console.log(under)  // This should print "2. Underdogs" in the console
