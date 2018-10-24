a4e8aecb7daf709fdbf6b34ab5d3cfbd00c0ded5
Lindsey Maddox <dev@lindseymaddox.com>
2018-10-23 20:09:51 -0500

    added HELP comment. Hopefully Ill be able to track those and come back to them in the future.

--- main.js
+++ main.js
@@ -15,3 +15,8 @@ console.log(scum)  // This should print "1. Galactic Scum" in the console
 
 const under = takeNumber("Underdogs")
 console.log(under)  // This should print "2. Underdogs" in the console
+
+
+
+
+//  HELP: not sure how to go about this challenge. 
